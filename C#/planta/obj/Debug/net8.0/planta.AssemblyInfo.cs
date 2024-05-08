f72734d0614cda322d0198b631363c89309b4d77
Victor Ezaki <victorezaki19@gmail.com>
2024-05-08 20:50:04 -0300

    Adicionando atividades de cpp

--- C#/planta/obj/Debug/net8.0/planta.AssemblyInfo.cs
+++ C#/planta/obj/Debug/net8.0/planta.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("planta")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87074573f3bf61678526e63ba2ebbfda4a1017ea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+788c4a80bcef79f8cbe4898cae3a700ab1fcb446")]
 [assembly: System.Reflection.AssemblyProductAttribute("planta")]
 [assembly: System.Reflection.AssemblyTitleAttribute("planta")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
