ec12c6ef8dad3f864daa33272d8252253ea17449
Victor Ezaki <victorezaki19@gmail.com>
2024-05-08 16:57:50 -0300

    Add looping

--- C#/planta/Program.cs
+++ C#/planta/Program.cs
@@ -4,27 +4,34 @@
         {
             Casa imovelC = new Casa();
             Apart imovelAp = new Apart();
+            string op = "s";
 
-            Console.WriteLine("\n\t\tSeja bem-vindo(a)\n");
-            Console.WriteLine("Para prosseguirmos com a construção do seu imóvel, precisamos de algumas \ninformações para podermos fazer a planta do mesmo.");
-            Console.WriteLine("------------------------------------------------------------------------------");
-
-            imovelC.InfoBasica();
-
-            switch (imovelC.CasaOuAp)
+            do
             {
-                case 1:
-                    imovelC.Coleta();
-                    break;
-                
-                case 2:
-                    imovelAp.Coleta();
-                    break;
-                
-                default:
-                    Console.WriteLine("Opção inválida!");
-                    break;
-            }
+                Console.WriteLine("\n\t\tSeja bem-vindo(a)\n");
+                Console.WriteLine("Para prosseguirmos com a construção do seu imóvel, precisamos de algumas \ninformações para podermos fazer a planta do mesmo.");
+                Console.WriteLine("------------------------------------------------------------------------------");
+
+                imovelC.InfoBasica();
+
+                switch (imovelC.CasaOuAp)
+                {
+                    case 1:
+                        imovelC.Coleta();
+                        break;
+                    
+                    case 2:
+                        imovelAp.Coleta();
+                        break;
+                    
+                    default:
+                        Console.WriteLine("Opção inválida!");
+                        break;
+                }
+
+                Console.Write("Deseja cadastrar outra planta (s/n)?");
+                op = Console.ReadLine();
+            } while(op == "s");
         }
     }
 }
