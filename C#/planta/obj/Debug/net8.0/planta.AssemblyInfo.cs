17fed3e5839e5da73a23a7897e0f5d473c21b6b0
Victor Ezaki <victorezaki19@gmail.com>
2024-05-23 22:45:43 -0300

    Aplicando MVC

--- C#/planta/obj/Debug/net8.0/planta.AssemblyInfo.cs
+++ C#/planta/obj/Debug/net8.0/planta.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("planta")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+200584d6c45c3d023303bd6910e38e7b5b331cfb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1bc9a95647d05fb893da9ad91b6b488fe8758037")]
 [assembly: System.Reflection.AssemblyProductAttribute("planta")]
 [assembly: System.Reflection.AssemblyTitleAttribute("planta")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
